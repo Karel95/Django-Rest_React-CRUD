733d786008ba883922993c17cb5093362f2a7c69
Karel <karelh2207@gmail.com>
2024-12-03 15:26:59 -0300

    updating

--- client/src/api/tasks.api.ts
+++ client/src/api/tasks.api.ts
@@ -1,11 +1,43 @@
 import axios from 'axios';
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+}
+
+interface NewTask {
+  title: string;
+  description: string;
+}
+
+const tasksApi = axios.create({
+  baseURL: 'http://localhost:8000/tasks/api/v1/tasks/',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   withCredentials: true, // to send cookies with requests
+//  'maxRedirects': 0, // to avoid infinite redirects
+})
 
 export function getAllTasks() {
   return (
-    axios.get('http://localhost:8000/tasks/api/v1/tasks')
+    // axios.get('http://localhost:8000/tasks/api/v1/tasks/')
+    //  .then(response => response.data)
+    //  .catch(error => console.log(error))
+    tasksApi.get<Task[]>('/')
      .then(response => response.data)
      .catch(error => console.log(error))
   )
 }
 
+export function createTask (task: NewTask) {
+  return (
+    // axios.post('http://localhost:8000/tasks/api/v1/tasks/', task)
+    //  .then(response => response.data)
+    //  .catch(error => console.log(error))
+    tasksApi.post('/', task)
+     .then(response => response.data)
+     .catch(error => console.log(error))
+  )
+}
--- client/src/pages/TaskFormPage.tsx
+++ client/src/pages/TaskFormPage.tsx
@@ -1,6 +1,57 @@
+import { useForm } from "react-hook-form";
+// otras bibliotecas recomendadas para usar junto a 'react-hook-form':
+// yup
+// zod
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
 
-export function TaskFormPage () {
+interface NewTask {
+  title: string;
+  description: string;
+}
+
+export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewTask>();
+
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async data => {
+    // // Enviar los datos al API para guardar la tarea
+    await createTask(data)
+    // // Redireccionar a la página de tareas
+    // window.location.href = "/tasks";
+    navigate("/tasks");
+    // // Opcionalmente, mostrar un mensaje de éxito al guardar la tarea
+    alert("Task created successfully!");
+  });
+  // // Validaciones personalizadas con 'react-hook-form'
+  // const validationSchema = yup.object().shape({
+  //   title: yup.string().required("Title is required"),
+  //   description: yup.string().required("Description is required"),
+  // });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>Title is required!</span>}
+        <textarea
+          id=""
+          rows={3}
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>Description is required!</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
