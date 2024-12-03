16654657aa5c3806e5557a37a0398e85a4a3c7ea
Karel <karelh2207@gmail.com>
2024-12-03 11:21:12 -0300

    updating

--- client/src/components/TasksList.tsx
+++ client/src/components/TasksList.tsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllTasks } from "../api/tasks.api";
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+}
+
 export function TasksList() {
+  const [tasks, setTasks] = useState<Task[]>([])
   useEffect(() => {
     async function loadTasks() {
       const res = await getAllTasks()
+      setTasks(res)
       console.log(res)
     }
     loadTasks()
   }, []);
 
-  return <div>TasksList</div>;
+  return <div>
+    {tasks.map(task => (
+      <div key={task.id}>
+        {task.title} - {task.description}
+      </div>
+    ))}
+    </div>;
 }
